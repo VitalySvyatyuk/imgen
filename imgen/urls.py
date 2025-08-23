0f05e9d9579394e1cbc47a3bf26f84760fe49dc5
Vitaly Svyatyuk <vitalysvyatyuk@gmail.com>
2025-08-23 17:06:46 +0300

    Fix static

--- imgen/urls.py
+++ imgen/urls.py
@@ -24,5 +24,8 @@ from generator.views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
